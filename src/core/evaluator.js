5abd6db3daccf333e0805afdf4cb63e558b588c9
Martynas Bagdonas <martbgd@gmail.com>
2019-06-14 19:32:57 +0300

    Fix rotation calculation

--- src/core/evaluator.js
+++ src/core/evaluator.js
@@ -1860,16 +1860,15 @@ var PartialEvaluator = (function PartialEvaluatorClosure() {
   
           let charWidth = textChunk.width + width - prevWidth;
           let charHeight = textState.fontSize;
+          let m = Util.transform(textState.ctm, textState.textMatrix);
           let rect = Util.getAxialAlignedBoundingBox(
-            [0, 0, charWidth, charHeight],
-            Util.transform(textState.ctm, textState.textMatrix)
-          );
+            [0, 0, charWidth, charHeight], m);
   
           textChunk.chars.push({
             c: glyphUnicode,
             rect,
             fontSize: textState.fontSize * textChunk.textAdvanceScale,
-            rotation: matrixToDegrees(textState.textMatrix)
+            rotation: matrixToDegrees(m)
           });
           
           textState.translateTextMatrix(tx, ty);

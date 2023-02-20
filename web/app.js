294c8ebd6e6028feca18b305801dd66c90a632ae
Martynas Bagdonas <git.martynas@gmail.com>
2023-02-20 16:57:29 +0000

    Allow overriding password prompt

--- web/app.js
+++ web/app.js
@@ -982,7 +982,7 @@ const PDFViewerApplication = {
     const loadingTask = getDocument(params);
     this.pdfLoadingTask = loadingTask;
 
-    loadingTask.onPassword = (updateCallback, reason) => {
+    loadingTask.onPassword = this.onPassword || ((updateCallback, reason) => {
       if (this.isViewerEmbedded) {
         // The load event can't be triggered until the password is entered, so
         // if the viewer is in an iframe and its visibility depends on the
@@ -993,7 +993,7 @@ const PDFViewerApplication = {
       this.pdfLinkService.externalLinkEnabled = false;
       this.passwordPrompt.setUpdateCallback(updateCallback, reason);
       this.passwordPrompt.open();
-    };
+    });
 
     loadingTask.onProgress = ({ loaded, total }) => {
       this.progress(loaded / total);

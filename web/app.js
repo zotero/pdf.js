b73deb56479588467bdcc9fd78428132f5adcc1d
Martynas Bagdonas <git.martynas@gmail.com>
2025-08-20 16:21:18 +0300

    Disable auto print action

--- web/app.js
+++ web/app.js
@@ -1615,7 +1615,7 @@ const PDFViewerApplication = {
       () => {
         this._unblockDocumentLoadEvent();
 
-        this._initializeAutoPrint(pdfDocument, openActionPromise);
+        // this._initializeAutoPrint(pdfDocument, openActionPromise);
       },
       reason => {
         this._documentError("pdfjs-loading-error", { message: reason.message });
